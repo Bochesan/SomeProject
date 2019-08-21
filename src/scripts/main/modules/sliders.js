353824c023ee91e26d2d4b55dbf2b2655f1030e4
Bochesan <bochesan89@yandex.ru>
2019-08-21 13:56:29 +0300

    serviceSection, wpsSection

--- src/scripts/main/modules/sliders.js
+++ src/scripts/main/modules/sliders.js
@@ -1,5 +1,6 @@
 var wwdSlider = document.querySelector('.wwdSection__slider');
-new Swiper(wwdSlider,{
+if (wwdSlider) {
+    new Swiper(wwdSlider,{
       effect: 'fade',
       fadeEffect: {
         crossFade: true
@@ -9,3 +10,23 @@ new Swiper(wwdSlider,{
       },
       speed: 4000,
     });
+}
+
+var wpsSlider = document.querySelector('.wpsSection__slider');
+if (wpsSlider) {
+    new Swiper(wpsSlider,{
+      effect: 'fade',
+      fadeEffect: {
+        crossFade: true
+      },
+      // autoplay: {
+      //   delay: 4000,
+      // },
+      speed: 2000,
+      autoHeight: true,
+      navigation: {
+          nextEl: ".wpsSection__slider .swiper-button-next",
+          prevEl: ".wpsSection__slider .swiper-button-prev",
+      }
+    });
+}
