a88bca26233a362772d252a245ac45171b1bcf04
Bochesan <bochesan89@yandex.ru>
2019-08-21 17:35:32 +0300

    wpsSection

--- src/scripts/main/modules/sliders.js
+++ src/scripts/main/modules/sliders.js
@@ -19,14 +19,25 @@ if (wpsSlider) {
       fadeEffect: {
         crossFade: true
       },
-      // autoplay: {
-      //   delay: 4000,
-      // },
-      speed: 2000,
+      loop: true,
+      autoplay: {
+        delay: 4000,
+      },
+      speed: 4000,
       autoHeight: true,
       navigation: {
           nextEl: ".wpsSection__slider .swiper-button-next",
           prevEl: ".wpsSection__slider .swiper-button-prev",
+      },
+      on: {
+          slideNextTransitionStart: function() {
+              wpsSlider.classList.remove('prevSlide');
+              wpsSlider.classList.add('nextSlide');
+          },
+          slidePrevTransitionStart: function() {
+              wpsSlider.classList.remove('nextSlide');
+              wpsSlider.classList.add('prevSlide');
+          }
       }
     });
 }
